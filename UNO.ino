bbde31aec620494d8247e66199b35bf190d6d833
5456465tony97202 <80087058+5456465tony97202@users.noreply.github.com>
2023-12-07 12:58:55 +0800

    Update UNO.ino

--- UNO.ino
+++ UNO.ino
@@ -1,27 +1,56 @@
-#include <Arduino.h>
+#include <Servo.h>
 #include <OneWire.h>
 #include <DallasTemperature.h>
-#include <PID_v1_bc.h>
 
-int pHSense = 4;            // pH 傳感器連接到模擬輸入 A0
-#define ONE_WIRE_BUS 3       // DS18B20 傳感器連接的數據引腳
+// 伺服馬達
+Servo servo_pin_9;
+boolean servoRotationRequested = false; // 用於手動模式下的旋轉控制
 
+// pH、DS18B20水溫
+int pHSense = 4;
+int samples = 10;
+float adc_resolution = 1023.0;
+
+
+#define ONE_WIRE_BUS 3
 OneWire oneWire(ONE_WIRE_BUS);
 DallasTemperature sensors(&oneWire);
 
-int samples = A1;
-float adc_resolution = 1023.0;
+
 
 void setup() {
     Serial.begin(9600);
+
+    // 伺服馬達
+    servo_pin_9.attach(9);
+
+    // pH、DS18B20水溫
     sensors.begin();
+
 }
 
-float ph(float voltage) {
-    return (5.5 - (voltage - 3.5) * 1.5);
-}
 
 void loop() {
+    if (Serial.available() > 0) {
+        char incomingChar = Serial.read();
+        if (incomingChar == 'r') {
+            // 手動模式下的旋轉控制，只有在手動模式下且未有旋轉請求時有效
+            if (!servoRotationRequested) {
+                servoRotationRequested = true;
+                servo_pin_9.write(90); // 以手動方式旋轉伺服馬達到 90 度位置
+                delay(3000);
+                servo_pin_9.write(0); // 返回到0度
+                servoRotationRequested = false;
+                Serial.println("Feed: on");
+            }
+        } else {
+            Serial.println("Feed: off");
+        }
+    }
+
+    Serial.print("/////////////////");
+    Serial.println();
+
     int pHReadings = 0;
 
     for (int i = 0; i < samples; i++) {
@@ -32,13 +61,10 @@ void loop() {
     float pHVoltage = 5.0 / adc_resolution * pHReadings / samples;
     float calculated_pH = ph(pHVoltage);
 
-    Serial.print("pH: "); // 添加 pH 标签
+    Serial.print("pH: ");
     Serial.println(calculated_pH, 2);
 
-    // 请求温度数据
     sensors.requestTemperatures();
-
-    // 读取温度数据
     float temperatureC = sensors.getTempCByIndex(0);
 
     Serial.print("Temp: ");
@@ -46,11 +72,9 @@ void loop() {
     Serial.print(" °C ");
     Serial.println();
 
-    //int input = analogRead(A1);
-
-    //Serial.print("WaterLevel: "); // 添加水位标签
-    //double waterLevelPercentage = (input / 1023.0) * 100;
-    //Serial.println(waterLevelPercentage);
-
     delay(10000);
+}
+
+float ph(float voltage) {
+    return (5.5 - (voltage - 3.5) * 1.5);
 }
